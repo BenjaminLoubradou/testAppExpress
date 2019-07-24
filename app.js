8ee5a7be36d6c1954a8ac05d9df40709a835c6c8
BenjaminLoubradou <loubradou.benjamin@hotmail.fr>
2019-07-24 14:37:43 +0200

    request and route GET

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// datas = {
+//     0: {
+//         "userId": "34", 
+//         "userName": "Hector"
+//     },
+//     1: {
+//         "userId": "78", 
+//         "userName": "victor"
+//     }
+// }
+
+
+
+
 app.get('/', (req, res) => res.send('Hello world'))
+
+app.get('/users', (req, res) => res.send(datas))
+
+app.get('/users/:userId/books/:bookId', (req,res) => res.send(req.params))
+
+app.get('/users/:name.:city', (req,res) => res.send(req.params))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
